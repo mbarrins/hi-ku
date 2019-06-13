154e9dff1b4ac53bed31c1074cd7a991649aead6
mbarrins <maija.barrins@gmail.com>
2019-06-13 23:19:06 +0100

    corrected top_poems method and corrected poems show page to refer to author instead of user

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -3,6 +3,6 @@ class Mood < ApplicationRecord
   has_many :authors, through: :poems
 
   def top_poems(no_of_poems)
-    self.poems.mood.poems.joins(:likes).group(:id).order(Arel.sql('COUNT(likes.id) DESC')).limit(no_of_poems)
+    self.poems.order(likes_count: :desc).limit(no_of_poems)
   end
 end
