dc0d0c6ab569a6c7d899632dcd424d5c664f88b3
mbarrins <maija.barrins@gmail.com>
2019-06-13 15:09:15 +0100

    added associations

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -2,4 +2,8 @@ class Poem < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   belongs_to :mood
+  has_many :likes
+  has_many :users, through: :likes
+  has_many :comments
+  has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :poems
+  has_many :genres, through: :poems
+  has_many :moods, through: :poems
+  has_many :likes
+  has_many :poems, through: :likes
+  has_many :comments
+  has_many :poems, through: :comments
 end
