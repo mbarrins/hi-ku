8b3cc70aecb6b74ab84f2b3f24dfc777b1032f21
JackH17 <jack.hawkins77@googlemail.com>
2019-06-17 10:50:04 +0100

    navbar start

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
+  get '/static/about', to: 'static#show', as: :about
+
+
   resources :comments, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :likes, only: [:create, :destroy]
   resources :poems
   resources :moods, only: [:index, :show]
   resources :genres, only: [:index, :show]
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
